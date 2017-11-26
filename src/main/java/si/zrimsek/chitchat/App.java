c8546c222f2382e5b7782d954b882de65aa25b9a
Ursa <ursazrimsek@gmail.com>
2017-11-26 14:38:11 +0100

    Dodane možnosti prijave, odjave, seznama uporabnikov

--- src/main/java/si/zrimsek/chitchat/App.java
+++ src/main/java/si/zrimsek/chitchat/App.java
@@ -1,21 +1,65 @@
 package si.zrimsek.chitchat;
 
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Request;
+import org.apache.http.client.utils.URIBuilder;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Hello ChitChat!
  */
 public class App {
-    public static void main(String[] args) {
-        try {
-            String hello = Request.Get("http://chitchat.andrej.com")
-                                  .execute()
-                                  .returnContent().asString();
-            System.out.println(hello);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws ClientProtocolException, IOException, URISyntaxException {
+    	String hello = Request.Get("http://chitchat.andrej.com")
+    						.execute()
+                            .returnContent().asString();
+        System.out.println(hello);
     }
+    
+    
+    public static void seznamUporabnikov() throws ClientProtocolException, IOException {
+    	String uporabniki = Request.Get("http://chitchat.andrej.com/users")
+								.execute()
+								.returnContent()
+								.asString();
+	    
+    	System.out.println(uporabniki);
+    }
+    
+    
+    public static void prijava(String uporabnik) throws URISyntaxException, ClientProtocolException, IOException {
+	    try {	
+    		URI uri = new URIBuilder("http://chitchat.andrej.com/users")
+	    				.addParameter("username", uporabnik)
+	    				.build();
+	    	String responseBody = Request.Post(uri)
+	                         		.execute()
+	                         		.returnContent()
+	                         		.asString();
+	
+	    	System.out.println(responseBody);
+	    }
+	    catch (Exception HttpResponseException) {
+	    	System.out.println("To uporabniško ime je že zasedeno");
+	    }
+    }
+    
+    
+    public static void odjava(String uporabnik) throws URISyntaxException, ClientProtocolException, IOException {
+    	URI uri = new URIBuilder("http://chitchat.andrej.com/users")
+		          .addParameter("username", uporabnik)
+		          .build();
+		String responseBody = Request.Delete(uri)
+		                               .execute()
+		                               .returnContent()
+		                               .asString();
+		System.out.println(responseBody);
+    }
+    
+
+    
+    
 }
